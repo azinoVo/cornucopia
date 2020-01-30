a4a8aac4c137c266899d942d73570980331e7143
azinoVo <azinovo22@gmail.com>
2020-01-29 18:23:15 -0600

    added home route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import GameTabs from './components/GameTabs';
+import Home from './components/Home';
 import Orchard from './components/Orchard';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <GameTabs />
 
+      <Route exact path="/" component={Home} />
       <Route path="/orchard" component={Orchard} />
     </div>
   );
