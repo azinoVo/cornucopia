d67c7758ae290d9a8adc069be936295669c36ea9
azinoVo <azinovo22@gmail.com>
2020-01-31 19:28:11 -0600

    added and imported all tabLinks

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import GameTabs from './components/GameTabs';
-import Home from './components/Home';
-import Orchard from './components/Orchard';
 import Feeder from './components/Feeder';
 import GeneralShop from './components/GeneralShop';
+import GoSculptures from './components/GoSculptures';
+import Greenhouse from './components/Greenhouse';
+import HangingGarden from './components/HangingGarden';
+import Home from './components/Home';
+import Inventory from './components/Inventory';
+import MainGarden from './components/MainGarden';
+import Orchard from './components/Orchard';
 
 
 function App() {
@@ -15,6 +20,14 @@ function App() {
       <GameTabs />
 
       <Route exact path="/" component={Home} />
+      <Route path="/feeder" component={Feeder} />
+      <Route path="/shop" component={GeneralShop} />
+      <Route path="/sculpture" component={GoSculptures} />
+      <Route path="/greenhouse" component={Greenhouse} />
+      <Route path="/hanging" component={HangingGarden} />
+      <Route path="/greenhouse" component={Greenhouse} />
+      <Route path="/inventory" component={Inventory} />
+      <Route path="/main" component={MainGarden} />
       <Route path="/orchard" component={Orchard} />
     </div>
   );
