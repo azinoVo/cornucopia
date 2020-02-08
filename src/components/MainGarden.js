eb68e5b29266b5ceb9535650998eb54c3520650d
azinoVo <azinovo22@gmail.com>
2020-02-07 22:49:45 -0600

    added plots without style without server

--- src/components/MainGarden.js
+++ src/components/MainGarden.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import adult_tree from '../assets/plants/Green Tree with Flowers.png';
 
 export default class MainGarden extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            plot: [adult_tree, adult_tree, undefined, undefined, undefined, undefined]
+        };
+    }
     render() {
         return (
             <section className='main-content'>
                 <div className='main'>
-                    Main Garden
+                    {this.state.plot.map(plot => {
+                        if(plot) {
+                            return <div className='plot'><img src={plot} alt={plot}/></div>
+                        } else {
+                            return <div className='plot'>Makeshift Plot</div>
+                        }
+                    })}
                 </div>
             </section>
         );
