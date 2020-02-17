548e7fc5543ad6215c9b588f285dedba479d8464
azinoVo <azinovo22@gmail.com>
2020-02-17 13:02:56 -0600

    added falling seed

--- src/components/MainGarden.js
+++ src/components/MainGarden.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import adult_tree from '../assets/plants/Green Tree with Flowers.png';
-import empty_plot from '../assets/plants/Empty Plot.png'
+import empty_plot from '../assets/plants/Empty Plot.png';
+import seed_down from '../assets/plants/Seed Down.gif';
 
 export default class MainGarden extends Component {
     constructor() {
         super();
         this.state = {
-            plot: [adult_tree, adult_tree, empty_plot, empty_plot, empty_plot, empty_plot]
+            plot: [adult_tree, adult_tree, empty_plot, seed_down, empty_plot, empty_plot]
         };
     }
 
