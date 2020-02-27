d93f20b18f83d06cd101b94ec2d7fd6f34b8f578
azinoVo <azinovo22@gmail.com>
2020-02-26 18:01:33 -0600

    added the game log not responsive yet

--- src/components/GameLog.js
+++ src/components/GameLog.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
 const GameLog = ({ log }) => {
+    const [gameLog, setLog] = useState([])
+
+    useEffect(() => {
+        if (log.length > 5) {
+            const sliced = log.slice(Math.max(log.length - 5, 0))
+            setLog(sliced)
+        } else {
+            setLog(log)
+        }
+    }, [log])
 
     return (
         <div className='game-log'>
             <h2 className='tab-header'>Game Log</h2>
             <div className='log'>
-                TESTING THE LOG
+                <ul>
+                    {
+                        gameLog.map(entry => {
+                            return <li>{entry}</li>
+                        })
+                    }
+                </ul>
             </div>
         </div>
     );
 }
 
-export default GameLog;
+const mapStateToProps = state => ({
+    log: state.game.log,
+});
+
+export default connect(mapStateToProps, {})(GameLog);
