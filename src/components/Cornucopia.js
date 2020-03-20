e8abbb9ac767da6ccdbf79d037e3f221342a8d6b
azinoVo <azinovo22@gmail.com>
2020-03-20 14:33:36 -0500

    added feature ideas

--- src/components/Cornucopia.js
+++ src/components/Cornucopia.js
@@ -15,8 +15,10 @@ const Cornucopia = () => {
                     <div className='deity-actions'>
                         <NumberModal />
 
-
+                        {/* Possible feature where user chooses what to offer the tree and get rewards. */}
                         <button>Action II</button>
+
+                        {/* Feature that changes the time of day immediately at the cost of some favor. */}
                         <button>Action III</button>
                     </div>
                 </div>
