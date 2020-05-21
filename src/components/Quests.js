c594c01e40c07bd65a45a2b7dcc132ecb3fc7483
azinoVo <azinovo22@gmail.com>
2020-05-20 23:40:59 -0500

    added some quest ideas

--- src/components/Quests.js
+++ src/components/Quests.js
@@ -4,6 +4,13 @@ const Quests = () => {
     return (
         <section className='quest-container'>
             {/* THis will store the quest system of the game, a set number of quests that will let user progress the game. */}
+            
+            {/* Have 10000 mana essence in your inventory */}
+
+            {/* Harvest a _____ from Main Garden. */}
+
+            {/* Harvest a _____ from Orchard. */}
+
         </section>
     );
 }
