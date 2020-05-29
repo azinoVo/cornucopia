b586abea0423dfadcd073211b12f58d0373b9b65
azinoVo <azinovo22@gmail.com>
2020-05-29 15:22:21 -0500

    added some stat controls

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 const Character = ({ log }) => {
+    const [statPoints, setStatPoints] = useState(30)
+    const [constitution, setConstitution] = useState(0)
+    const [attack, setAttack] = useState(0)
+    const [defense, setDefense] = useState(0)
+    const [dexterity, setDexterity] = useState(0)
+    const [intelligence, setIntelligence] = useState(0)
+    const [speed, setSpeed] = useState(0)
+
+    controlStats = (control, stat) => {
+        // control will be either increase or decrease
+        // stat is the stat that will be controlled
+        console.log("Within controlStats")
+
+    }
 
     useEffect(() => {
         console.log("This is the character creation component.")
@@ -11,7 +25,11 @@ const Character = ({ log }) => {
     <div>
         <h1>This is the character creation page.</h1>
         <div className='stats'>
-            <div>Constitution</div>
+            <div>
+                <button>REDUCE</button>
+                <span>Constitution</span>
+                <button>Increase</button>
+            </div>
             <div>Attack</div>
             <div>Defense</div>
             <div>Dexterity</div>
