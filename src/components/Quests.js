86fb90cf4277898eb17bf2e11aa8faf2fdbb2e5b
azinoVo <azinovo22@gmail.com>
2020-07-16 20:19:02 -0500

    added some quests

--- src/components/Quests.js
+++ src/components/Quests.js
@@ -3,25 +3,34 @@ import React from 'react';
 const Quests = () => {
     return (
         <section className='quest-container'>
-            {/* THis will store the quest system of the game, a set number of quests that will let user progress the game. */}
-            
-            {/* Have 10000 mana essence in your inventory */}
+            {/* This will store the quest system of the game, a set number of quests that will let user progress the game. */}
+            <ul>
+                {/* Have 10000 mana essence in your inventory */}
+                <li>Mana Addict: Have 10,000 Mana Essence in your inventory.</li>
 
-            {/* Harvest a _____ from Main Garden. */}
+                {/* Have 10000 water capacity. */}
+                <li>Thirsty: Achieve 5000 Water Capacity.</li>
 
-            {/* Harvest a _____ from Orchard. */}
+                {/* Harvest _____ crops. This may need a counter within the store. */}
+                <li>Master of Crops: Harvest 100 crops within the Main Garden.</li>
 
-            {/* Harvest _____ crops. This may need a counter within the store. */}
+                {/* Harvest _____ fruits from trees. Also require a counter in store. */}
+                <li>Master of the Orchard: Harvest 100 fruits within the Orchard.</li>
 
-            {/* Harvest _____ fruits from trees. Also require a counter in store. */}
+                {/* Have maximum number of garden plots. */}
+                <li>Spacious Garden: Unlock six Main Garden plots.</li>
 
-            {/* Have maximum number of garden plots. */}
+                {/* Have maximum number of orchard plots. */}
+                <li>Spacious Orchard: Unlock six Orchard plots.</li>
 
-            {/* Have maximum number of orchard plots. */}
+                {/* Have all Hearts within your inventory. Reward possibly a massive amount of money and a new feature. */}
 
-            {/* Have all Hearts within your inventory. Reward possibly a massive amount of money and a new feature. */}
+                {/* Harvest a _____ from Main Garden. */}
 
-            {/* Have 10000 water capacity. */}
+                {/* Harvest a _____ from Orchard. */}
+            </ul>
+
+
 
         </section>
     );
